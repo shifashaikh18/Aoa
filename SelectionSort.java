1a3f0fd8e89abe1d1a228f5536ac6fcb0464bbdc
shifashaikh18 <shifa.s2409487209@vcet.edu.in>
2025-02-12 16:55:41 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,19 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
